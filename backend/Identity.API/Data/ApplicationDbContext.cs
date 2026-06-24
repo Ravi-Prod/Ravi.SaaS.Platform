b55d6bd03579d2e87d17a9db088bf8b1bc62a2d7
Ravi-Prod <ravi.bg4@gmail.com>
2026-06-24 20:46:58 +0530

    Add Identity.API project

---
+++ backend/Identity.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Identity.API.Entities;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.API.Data;
+
+public class ApplicationDbContext :
+    IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+    public DbSet<Permission> Permissions => Set<Permission>();
+
+    public DbSet<RolePermission> RolePermissions => Set<RolePermission>();
+    }
---
+++ backend/Identity.API/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API.Entities;
+
+public class ApplicationUser : IdentityUser
+    {
+    public Guid TenantId { get; set; }
+
+    public Guid BranchId { get; set; }
+
+    public string FullName { get; set; } = "";
+    }
